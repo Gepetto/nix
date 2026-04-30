8d8c0f8b18c7725fee4e5c0df4e72d4e0f1d352c
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-30 16:06:17 +0200

    flex-joinst: standalone

--- py-pkgs/flex-joints.nix
+++ py-pkgs/flex-joints.nix
@@ -1,10 +1,35 @@
 {
+  lib,
   toPythonModule,
   pkgs,
+
+  boost,
+  eigenpy,
+  python,
+  pythonImportsCheckHook,
+
+  buildStandalone ? true,
 }:
 toPythonModule (
-  pkgs.flex-joints.override {
-    inherit (pkgs) python3Packages;
-    pythonSupport = true;
-  }
+  pkgs.flex-joints.overrideAttrs (super: {
+    pname = "py-${super.pname}";
+    cmakeFlags = super.cmakeFlags ++ [
+      (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+      (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" buildStandalone)
+    ];
+    nativeBuildInputs = [
+      python
+    ]
+    ++ super.nativeBuildInputs;
+
+    propagatedBuildInputs = [
+      boost
+      eigenpy
+      pythonImportsCheckHook
+    ]
+    ++ super.propagatedBuildInputs
+    ++ lib.optional buildStandalone pkgs.flex-joints;
+
+    pythonImportsCheck = [ "flex_joints" ];
+  })
 )
